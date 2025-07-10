0f6e7e8ce9868304d732ab3412a192758ca7907b
Goodfellow36 <troy@teamgiveandgo.com>
2025-07-09 23:45:50 -0500

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export interface Mentor {
+  id: string;
+  name: string;
+  university: string;
+  position: string;
+  year: string;
+  price: number;
+  rating: number;
+  reviewCount: number;
+  location: {
+    city: string;
+    state: string;
+  };
+  bio: string;
+  achievements: string[];
+  avatar: string;
+  available: boolean;
+  sessionLength: number;
+  conference: string;
+}
+
+export interface LocationFilter {
+  state: string;
+  city: string;
+  distance: number;
+}
